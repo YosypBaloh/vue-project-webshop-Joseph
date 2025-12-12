8fe8bd9e78871a750ffd4858529adc0ff8b6eb0c
Balog Csalad <a_regisztralt_email_cmed@example.com>
2025-12-12 22:08:04 +0200

    vue-general-functions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-    <h1>{{ title }}</h1>
-    <p>{{ isHasCar }}</p>
-</template>
+    <h1 v-on:click="changeText()">{{ title.toLowerCase() }}!</h1>
+    <p @mouseover="changeText('Some new')">{{ !isHasCar }}</p>
+    <p v-bind:style="style">{{ info }}</p>
+    <input @input="changeTextDynamic($event.target.value)" /> <br />
+    <input @input="changeStyle($event.target.value)" />
+    </template>
 
 <script>
 export default {
@@ -9,9 +12,28 @@ data() {
     return {
         title: "Hello World",
         isHasCar: true,
+        userInput: "",
+        style: "",
+    };
+    },
+    methods: {
+        changeText(word = "New") {
+        this.title = word;
+    },
+    changeTextDynamic(value) {
+        this.userInput = value;
+    },
+    changeStyle(value) {
+        this.style = value;
+    },
+  },
+  computed: {
+    info() {
+        if (this.userInput === "");
+        else return "User text: " + this.userInput + "!";
     }
-}
-}
+  }
+};
 </script>
 
 <style scoped>
