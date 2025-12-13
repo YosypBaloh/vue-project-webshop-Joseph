10f6d3551927436e9b929687b10642d35afc8561
Joseph Balog <balohyosyp.96@gmail.com>
2025-12-13 16:37:04 +0200

    header.vue added

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,18 @@
 <template>
-    <h1 v-on:click="changeText()">{{ title.toLowerCase() }}!</h1>
-    <p @mouseover="changeText('Some new')">{{ !isHasCar }}</p>
-    <p v-bind:style="style">{{ info }}</p>
-    <input @input="changeTextDynamic($event.target.value)" /> <br />
-    <input @input="changeStyle($event.target.value)" />
-    </template>
+    <Header />
+</template>
 
 <script>
+import Header from './components/Header.vue';
+
 export default {
-data() {
-    return {
-        title: "Hello World",
-        isHasCar: true,
-        userInput: "",
-        style: "",
-    };
+    components: {
+        Header,
     },
-    methods: {
-        changeText(word = "New") {
-        this.title = word;
-    },
-    changeTextDynamic(value) {
-        this.userInput = value;
-    },
-    changeStyle(value) {
-        this.style = value;
-    },
-  },
-  computed: {
-    info() {
-        if (this.userInput === "");
-        else return "User text: " + this.userInput + "!";
-    }
-  }
 };
+
 </script>
 
-<style scoped>
+<style>
     
 </style>
